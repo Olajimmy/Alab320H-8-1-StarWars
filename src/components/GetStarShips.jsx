d8204163e307d604e92bf059969ec59ae2e139bf
olajimmy <overdedicated9@gmail.com>
2024-12-16 16:04:31 -0500

    fetch successful

--- src/components/GetStarShips.jsx
+++ src/components/GetStarShips.jsx
@@ -1,5 +1,40 @@
 //
+import { useEffect, useState } from "react";
+
 function GetStarships() {
-  return <div>this is start ships component</div>;
+  const [ships, setShips] = useState(null);
+
+  const url = `https://swapi.py4e.com/api/starships/9/`;
+  //console.log(url);
+  const getOneShip = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setShips(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getOneShip(url);
+  }, []);
+
+  return (
+    <div>
+      this is start ships component
+      {name}
+    </div>
+  );
 }
 export default GetStarships;
+
+// onst getOnePokemon = async (url) => {
+//     try {
+//       const response = await fetch(url);
+//       const data = await response.json();
+//       setPokemon(data);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
