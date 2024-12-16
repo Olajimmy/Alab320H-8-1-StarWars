8a8e01e762fcd86120def4da7826c27751f8a6ed
olajimmy <overdedicated9@gmail.com>
2024-12-16 15:20:19 -0500

    folder structure setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import GetStarships from "./components/GetStarShips";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <GetStarships />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/GetStarShips.jsx
@@ -0,0 +1,5 @@
+//
+function GetStarships() {
+  return <div>this is start ships component</div>;
+}
+export default GetStarships;
